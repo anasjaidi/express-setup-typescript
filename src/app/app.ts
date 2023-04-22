2e0917b00301bc5911bb1b343ff31e94dd70c7be
Anas jaidi <anas.jaidi@icloud.com>
2023-04-22 17:09:40 +0000

    add requests gateway

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,16 @@
-import express from "express";
+import express, { RequestHandler } from "express";
 import dotenv, { DotenvConfigOptions } from "dotenv";
 import morgan from "morgan";
 import cors from "cors";
 import path from "path";
 import appConfig from "../config/app.config";
+import helmet from "helmet";
 
 // append .env vars to envirement variables
 
 const appDotenvOptions: DotenvConfigOptions = {
-  path: '../../.env'
-}
+	path: "../../.env",
+};
 
 dotenv.config(appDotenvOptions);
 
@@ -34,6 +35,13 @@ app.set("env", name);
 app.use(cors());
 app.use(express.json());
 app.use(morgan(process.env.MORGAN_MODE!));
-
+app.use(helmet());
 // app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "./public"), { dotfiles: "ignore" }));
+app.use(
+	express.static(path.join(__dirname, "./public"), { dotfiles: "ignore" })
+);
+
+// start deafult route
+app.use("*", ((req, res, next) => {
+	next(); // TODO: add 404 error
+}) as RequestHandler);
