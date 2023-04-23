e09ca7c7e122f4ca7f77ef2cb163d5fa58f58957
Anas jaidi <anas.jaidi@icloud.com>
2023-04-23 22:51:31 +0100

    finish disk uploader Factory

---
+++ src/media/disk/conf/diskMedia.conf.ts
@@ -0,0 +1,70 @@
+import multer, { diskStorage, memoryStorage } from "multer";
+import { join } from "path";
+import AppError from "../../../app/errors/AppError";
+import {
+	MulterConfFilterCallBackType,
+	MulterStorageConfFunctionType,
+} from "./diskMediaConf";
+
+// TODO: read it from env vars
+
+const LOCATION = "MEMORY";
+const MAX_IMAGE_SIZE = 100 * 1024 * 1024;
+const IMAGES_LOCATION = "uploads";
+
+// start Storage Config Functions
+
+const multerDestinationFactory = (
+	fileLocation: string
+): MulterStorageConfFunctionType => {
+	return (req, file, cb) => {
+		cb(null, join(__dirname, "..", "..", "..", fileLocation));
+	};
+};
+
+const multerFileNameFactory = (
+	fileType: string
+): MulterStorageConfFunctionType => {
+	return (req, file, cb) => {
+		const ext = file.mimetype.split("/")[1];
+		cb(null, `${fileType}-${"req.user.uid"}-${Date.now()}.${ext}`);
+	};
+};
+
+const multerFileTypeFilter = (
+	mimeType: string
+): MulterConfFilterCallBackType => {
+	return (req, file, cb) => {
+		if (file.mimetype.startsWith("image/")) {
+			cb(null, true);
+		} else {
+			cb(new AppError(400, "Only images are allowed!") as Error, false);
+		}
+	};
+};
+
+const uploaderFactory = (
+	location: "MEMORY" | "DISK",
+	mimeType: string,
+	fileType: string,
+	fileLocation: string,
+	maxFileSize: number
+) => {
+	let storage: multer.StorageEngine;
+
+	if (location === "MEMORY") {
+		storage = memoryStorage();
+	} else {
+		storage = diskStorage({
+			destination: multerDestinationFactory(fileLocation) as unknown as string,
+			filename: multerFileNameFactory(fileType),
+		});
+	}
+	return multer({
+		storage,
+		fileFilter: multerFileTypeFilter(mimeType),
+		limits: { fileSize: maxFileSize },
+	});
+};
+
+export default uploaderFactory;
