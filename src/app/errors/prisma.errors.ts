13e3ac4b7569b713897ed17522de5fd21f6d95e1
Anas jaidi <anas.jaidi@icloud.com>
2023-04-23 04:46:32 +0100

    fix some prisma errs issues

--- src/app/errors/prisma.errors.ts
+++ src/app/errors/prisma.errors.ts
@@ -1,7 +1,7 @@
 import { EnvModesErrorDispatcher } from "../types/errors";
 
 const uniqueValueError : EnvModesErrorDispatcher = (err, res) => {
-	const message = err.meta.taget.join(", ") + " needs to be unique.";
+	const message = err.meta.target.join(", ") + " needs to be unique.";
 	res.status(409).json({
 		status: "fail",
 		message,
