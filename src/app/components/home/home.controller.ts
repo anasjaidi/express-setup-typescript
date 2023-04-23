04468c395b1ba2fc65fe58713b1145ecbeb8f3cc
Anas jaidi <anas.jaidi@icloud.com>
2023-04-23 03:54:52 +0100

    add home controller

---
+++ src/app/components/home/home.controller.ts
@@ -0,0 +1,11 @@
+import ErrorsWrapper from "../../errors/ErrorsWrapper";
+
+const homeController = ErrorsWrapper(async (req, res, next) => {
+  res.status(200).json({
+    status: "success"
+  })
+})
+
+const homeControllers = {homeController}
+
+export default homeControllers
--- src/app/types/errors.d.ts
+++ src/app/types/errors.d.ts
@@ -2,7 +2,7 @@ import { NextFunction, Response, Request, RequestHandler } from "express";
 
 
 
-export type ErrorsWrapperRequestHandlerParam = (fn: (req, res, next) => Promise<void>) => RequestHandler
+export type ErrorsWrapperRequestHandlerParam = (fn: (req : Request, res : Response, next: NextFunction) => Promise<void>) => RequestHandler
 
 export type EnvModesErrorDispatcher = (err: AppError, res: Response) => void
 
