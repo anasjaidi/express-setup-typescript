d94889ffb97a5f8bbf8936f69a40f3a909fb4502
Anas jaidi <anas.jaidi@icloud.com>
2023-04-23 04:04:04 +0100

    define auth validation schemas

---
+++ src/app/components/auth/auth.validation.ts
@@ -0,0 +1,18 @@
+import Joi from "joi";
+
+const userSignUpSchema = Joi.object().keys({
+	firstName: Joi.string().min(3).max(50).required(),
+	lastName: Joi.string().min(3).max(50).required(),
+	email: Joi.string().email().required(),
+	password: Joi.string().min(5).required(),
+	repeatpassword: Joi.ref("passowrd"),
+});
+
+const userSignInSchema = Joi.object().keys({
+	email: Joi.string().email().required(),
+	password: Joi.string().min(5).required(),
+});
+
+const authValidationSchemas = { userSignUpSchema, userSignInSchema };
+
+export default authValidationSchemas
