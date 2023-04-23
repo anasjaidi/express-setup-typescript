54786d2bc8a459bd6d043c45d95dd1eccf9dc9ac
Anas jaidi <anas.jaidi@icloud.com>
2023-04-23 01:22:43 +0000

    add app config

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,6 +4,16 @@ import path from "path";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import helmet from "helmet";
+import appConfigs from "../conf/app.config";
 
 // append .env vars to envirement variables
 dotenv.config({ path: "../../.env" });
+
+// select settings for choosen mode
+const env = process.env.NODE_ENV || "development";
+
+const {
+	corsOption,
+	name,
+	app: { port, debug, logger_format },
+} = appConfigs(env)!;
